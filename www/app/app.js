b6d3d5a23459d0be8d80645afdd9343014f59e5c
linsenss <linsen_ss@sina.com>
2016-03-10 18:17:35 +0800

    修改apk下载地址

--- www/app/app.js
+++ www/app/app.js
@@ -37,7 +37,7 @@ angular
                     if (versionCode < newVersionCode) {
                         ConfirmModalDialogService.AsyncConfirmYesNo("版本有更新，是否需要升级？",
                             function () {
-                                var url = "http://115.28.66.10:9090/cgwy_suppliers.apk";
+                                var url = "http://115.28.66.10:9090/download/cgwy_suppliers.apk";
                                 var targetPath = cordova.file.externalApplicationStorageDirectory + 'cgwy/cgwy_suppliers_' + newVersionCode + '.apk';
                                 var trustHosts = true;
                                 var options = {};
